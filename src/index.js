0e2d053e7839a8bcaf3433c62ad6935427e238d7
Olivia Tharp <olivia.tharp@nih.gov>
2022-01-06 15:31:03 -0600

    update to alpha 36

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.scss';
 
-import { UsaFooter } from '@nciocpl/ncids-js';
+import { NCIFooter, MyNCIFooter } from '@nciocpl/ncids-js';
 
 // create all accordions
 window.addEventListener('DOMContentLoaded', () => {
@@ -8,7 +8,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // loop through each footer found and init (should only be one footer, no looping needed IRL)
     [...elements].forEach((element) => {
-        const footer = UsaFooter.create(element, {trigger: '.usa-footer__nci-primary-link--accordion-header' });
+        // should not be allowed: const footer = new UsaFooter(element, {trigger: '.usa-footer__nci-primary-link--accordion-header' });
+        const footer = MyNCIFooter.create(element, { trigger: '.usa-footer__nci-primary-link--accordion-header' });
         console.log(footer);
 
         // when footer content toggles .hidden, do something
@@ -16,14 +17,19 @@ window.addEventListener('DOMContentLoaded', () => {
             'usa-footer:nav-links:collapse',
             (event) => {
                 console.log('collapsed', event);
-            }
+            },
         );
 
-        footer.element.addEventListener(
-            'usa-footer:nav-links:expand',
-            (event) => {
-                console.log('expanded', event);
-            }
-        );
+        footer.element.addEventListener('usa-footer:nav-links:expand', (event) => {
+            console.log('expanded', event);
+        });
+
+        footer.element.addEventListener('usa-footer:sign-up:submit', (event) => {
+            console.log('valid email', event);
+        });
+
+        footer.element.addEventListener('usa-footer:sign-up:error', (event) => {
+            console.log('invalid email', event);
+        });
     });
 });
