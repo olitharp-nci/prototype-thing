d697a8d7ab5aa2889588baacaa16176ce961fee6
Olivia Tharp <olivia.tharp@nih.gov>
2022-01-19 13:36:56 -0600

    update packge to alpha-40, add recreate on resize

--- src/index.js
+++ src/index.js
@@ -1,37 +1,29 @@
 import './styles.scss';
 
-// todo error "Module is not installed"
-import { NCIFooter, MyNCIFooter } from '@nciocpl/ncids-js';
+import { NCIFooter } from '@nciocpl/ncids-js';
 
-// create all accordions
-window.addEventListener('DOMContentLoaded', () => {
-    const elements = document.getElementsByClassName('usa-footer');
+const element = document.getElementById('nci-footer');
+const footer = NCIFooter.create(element, { trigger: '.usa-footer__nci-primary-link--accordion-header' });
+console.log(footer);
 
-    // loop through each footer found and init (should only be one footer, no looping needed IRL)
-    [...elements].forEach((element) => {
+footer.element.addEventListener('usa-footer:nav-links:collapse', (event) => {
+    console.log('collapsed', event);
+});
+
+footer.element.addEventListener('usa-footer:nav-links:expand', (event) => {
+    console.log('expanded', event);
+});
+
+footer.element.addEventListener('usa-footer:sign-up:submit', (event) => {
+    console.log('valid email', event);
+});
+
+footer.element.addEventListener('usa-footer:sign-up:error', (event) => {
+    console.log('invalid email', event);
+});
+
+window.addEventListener('resize', () => {
         const footer = NCIFooter.create(element, { trigger: '.usa-footer__nci-primary-link--accordion-header' });
         console.log(footer);
-
-        // when footer content toggles .hidden, do something
-        footer.element.addEventListener(
-            'usa-footer:nav-links:collapse',
-            (event) => {
-                console.log('collapsed', event);
-            },
-        );
-
-        footer.element.addEventListener('usa-footer:nav-links:expand', (event) => {
-            console.log('expanded', event);
-        });
-
-        footer.element.addEventListener('usa-footer:sign-up:submit', (event) => {
-            console.log('valid email', event);
-        });
-
-        footer.element.addEventListener('usa-footer:sign-up:error', (event) => {
-            console.log('invalid email', event);
-        });
-
-    });
-
-});
+    },
+);
